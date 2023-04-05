4ab2430a44493dcf6225917d0413a4cd86fb27ac
Tomasz Kasprowicz <tomasz.k@bitnoi.se>
2023-04-05 13:23:57 +0200

    SCHEDULER-1 tsconfig and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "react-scheduler",
+      fileName: "index"
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
+  }
 });
