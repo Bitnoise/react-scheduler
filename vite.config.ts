58b76d36f63386bac38f17dfae63c2e025286a8e
Piotr Baranek <piotr.b@bitnoi.se>
2023-04-07 12:58:13 +0200

    chore: Add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts(),
+    visualizer({
+      template: "treemap"
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
