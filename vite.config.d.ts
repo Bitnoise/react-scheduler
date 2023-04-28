554c4dcd45c2501c106d85c1d3fdcb3045ac3f02
Bartosz <bartosz.b@bitnoi.se>
2023-04-28 12:36:45 +0200

    chore: Test husky

--- vite.config.d.ts
+++ vite.config.d.ts
@@ -1,2 +1,3 @@
+// eslint-disable-next-line filenames/match-exported
 declare const _default: import("vite").UserConfigExport;
 export default _default;
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
