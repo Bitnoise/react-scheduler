9c9b7df1ac564de0945216c9953e753af0bf2b0d
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-03-07 08:00:42 -0300

    feat: lang pt-BR

---
+++ src/locales/ptBR.ts
@@ -0,0 +1,17 @@
+export const ptBR = {
+  feelingEmpty: "Sem dados...",
+  free: "livre",
+  loadNext: "Próximo",
+  loadPrevious: "Anterior",
+  over: "terminé",
+  taken: "ocupado",
+  topbar: {
+    filters: "filtros",
+    next: "próximo",
+    prev: "anterior",
+    today: "hoje",
+    view: "visualização"
+  },
+  search: "Procurar",
+  week: "Semana"
+};
