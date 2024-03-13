fc9d6fb0fa2a6fc6e8a6cf5cfd07b113bb4cd880
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-03-13 16:29:50 -0300

    0.1.8

--- src/mock/appMock.ts
+++ src/mock/appMock.ts
@@ -37,7 +37,7 @@ export const generateProjects = (
     .get("year");
 
   const data = [];
-  const bgColor =  "#D1FADF";
+  const bgColor = "#D1FADF";
 
   for (let yearIndex = startYear; yearIndex <= endYear; yearIndex++) {
     const projectsPerYear = Math.ceil(Math.random() * maxProjectsPerYear);
