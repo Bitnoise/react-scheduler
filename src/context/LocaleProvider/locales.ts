b982fa9285202e97fffbe435030e3b2c5a3e1d51
arek-bitnoise <arkadiusz.z@bitnoi.se>
2024-06-28 11:36:48 +0200

    feat: added IT langueage

--- src/context/LocaleProvider/locales.ts
+++ src/context/LocaleProvider/locales.ts
@@ -3,10 +3,17 @@ import plDayjsTranslations from "dayjs/locale/pl";
 import deDayjsTranslations from "dayjs/locale/de";
 import ltDayjsTranslations from "dayjs/locale/lt";
 import frDayjsTranslations from "dayjs/locale/fr";
-import { en, pl, de, lt, fr } from "@/locales";
+import itDayjsTranslations from "dayjs/locale/it";
+import { en, pl, de, lt, fr, it } from "@/locales";
 import { LocaleType } from "./types";
 
 export const localesData: LocaleType[] = [
+  {
+    id: "de",
+    lang: de,
+    translateCode: "de-DE",
+    dayjsTranslations: deDayjsTranslations
+  },
   {
     id: "en",
     lang: en,
@@ -14,10 +21,16 @@ export const localesData: LocaleType[] = [
     dayjsTranslations: enDayjsTranslations
   },
   {
-    id: "pl",
-    lang: pl,
-    translateCode: "pl-PL",
-    dayjsTranslations: plDayjsTranslations
+    id: "fr",
+    lang: fr,
+    translateCode: "fr-FR",
+    dayjsTranslations: frDayjsTranslations
+  },
+  {
+    id: "it",
+    lang: it,
+    translateCode: "it-IT",
+    dayjsTranslations: itDayjsTranslations
   },
   {
     id: "lt",
@@ -26,16 +39,10 @@ export const localesData: LocaleType[] = [
     dayjsTranslations: ltDayjsTranslations
   },
   {
-    id: "de",
-    lang: de,
-    translateCode: "de-DE",
-    dayjsTranslations: deDayjsTranslations
-  },
-  {
-    id: "fr",
-    lang: fr,
-    translateCode: "fr-FR",
-    dayjsTranslations: frDayjsTranslations
+    id: "pl",
+    lang: pl,
+    translateCode: "pl-PL",
+    dayjsTranslations: plDayjsTranslations
   }
 ];
 
