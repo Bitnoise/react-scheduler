43180188b86ce5b7b6543a5b03de5f784009f330
arek-bitnoise <arkadiusz.z@bitnoi.se>
2024-06-28 11:24:45 +0200

    feat: added FR lang

--- src/types/global.ts
+++ src/types/global.ts
@@ -9,7 +9,7 @@ type ZoomLevelTuple = typeof allZoomLevel;
 
 export type ZoomLevel = ZoomLevelTuple[number];
 
-export type LangCodes = "en" | "pl" | "de" | "lt";
+export type LangCodes = "en" | "pl" | "de" | "lt" | "fr";
 
 export type Config = {
   zoom: ZoomLevel;
@@ -21,7 +21,7 @@ export type Config = {
    */
   filterButtonState?: number;
   /**
-   * Language code: "en" | "pl" | "de"
+   * Language code: "en" | "pl" | "de" | "lt" | "fr"
    */
   lang?: LangCodes | string;
   isFiltersButtonVisible?: boolean;
