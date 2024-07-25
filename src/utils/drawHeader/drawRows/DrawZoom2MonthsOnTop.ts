3b367bfd9a8df8d3810417d33c20b1d1741e5e95
arek-bitnoise <arkadiusz.z@bitnoi.se>
2024-07-25 13:45:34 +0200

    display month name

--- src/utils/drawHeader/drawRows/DrawZoom2MonthsOnTop.ts
+++ src/utils/drawHeader/drawRows/DrawZoom2MonthsOnTop.ts
@@ -1,59 +1,33 @@
 import dayjs from "dayjs";
 import {
   fonts,
-  headerHeight,
-  headerMonthHeight,
-  headerWeekHeight,
-  monthsInYear,
+  leftColumnWidth,
+  outsideWrapperId,
+  screenWidthMultiplier,
   topRowTextYPos,
-  zoom2ColumnWidth,
   zoom2HeaderTopRowHeight
 } from "@/constants";
 import { Day } from "@/types/global";
 import { drawRow } from "../../drawRow";
 
-export const drawZoom2MonthsOnTop = (ctx: CanvasRenderingContext2D, cols, startDate: Day) => {
-  // let yearIndex = 0;
-  let index = dayjs(`${startDate.year}-${startDate.month + 1}-${startDate.dayOfMonth}`).month();
-  // xPos = -startDate.dayOfMonth * zoom2ColumnWidth + 0.5 * zoom2ColumnWidth;
+export const drawZoom2MonthsOnTop = (
+  ctx: CanvasRenderingContext2D,
+  cols: number,
+  startDate: Day
+) => {
+  const month = dayjs(`${startDate.year}-${startDate.month + 1}-${startDate.dayOfMonth + 1}`);
 
-  for (let i = 0; i < monthsInYear; i++) {
-    if (index > monthsInYear - 1) {
-      index = 0;
-      // yearIndex++;
-    }
+  const wrapperWidth = document.getElementById(outsideWrapperId)?.clientWidth || 0;
+  const componentWidth = wrapperWidth - leftColumnWidth;
 
-    const middleDay = dayjs(`${startDate.year}-${startDate.month + 1}-${startDate.dayOfMonth}`).add(
-      Math.ceil(cols / 2),
-      "hours"
-    );
-
-    const daysInMonth = dayjs(`${startDate.year}-${startDate.month + 1}-${startDate.dayOfMonth}`)
-      .add(i, "months")
-      .daysInMonth();
-
-    const month = dayjs(`${startDate.year}-${startDate.month}-${startDate.dayOfMonth}`).add(
-      i,
-      "months"
-    );
-
-    const width = 24 * daysInMonth * zoom2ColumnWidth;
-    let xPos =
-      -middleDay.hour() * zoom2ColumnWidth + i * 24 * zoom2ColumnWidth + 0.5 * zoom2ColumnWidth;
-    // width = 500;
-
-    drawRow({
-      ctx,
-      x: xPos,
-      y: 0,
-      width,
-      height: zoom2HeaderTopRowHeight,
-      textYPos: topRowTextYPos,
-      label: month.format("MMMM").toUpperCase(),
-      font: fonts.topRow
-    });
-
-    xPos += width;
-    index++;
-  }
+  drawRow({
+    ctx,
+    x: 0,
+    y: 0,
+    width: componentWidth * screenWidthMultiplier,
+    height: zoom2HeaderTopRowHeight,
+    textYPos: topRowTextYPos,
+    label: month.format("MMMM").toUpperCase(),
+    font: fonts.topRow
+  });
 };
